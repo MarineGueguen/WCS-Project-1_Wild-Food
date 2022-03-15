7e7c5128840f8fbb212be39a1f768796e03c6a0a
MARINE GUEGUEN <gueguenmarine.p7@gmail.com>
2022-03-15 20:32:18 +0100

    creation of the nav bar (responsive)

--- _header.php
+++ _header.php
@@ -1,5 +1,39 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="nav.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+    </style> 
+</head>
+
 <header>
-    <nav>
+    <nav class="nav-bar">
+
+        <div class="logo">
+            <img src="https://i.imgur.com/iheOsmy.png" alt="logo" class="img-logo"/>
+            <div class="text-logo">
+                <h2 class="blog-title">Wild food</h2>
+           
+</div>
+        </div>
+
+        <label>
+            <input type="checkbox">
+            <span class="menu"> <span class="hamburger"></span> </span>
+            <ul>
+                <li> <a href="#">Accueil</a> </li>
+                <li> <a href="#">Apéritifs</a> </li>
+                <li> <a href="#">Entrées</a> </li>
+                <li> <a href="#">Plats</a> </li>
+                <li> <a href="#">Desserts</a> </li>
+                <li> <a href="#">Ta recette ?</a> </li>
+            </ul>
+
+        </label>
+        
+           
 
     </nav>
 </header>
