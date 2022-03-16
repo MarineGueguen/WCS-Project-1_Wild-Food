eb55d97310d6a8e1b901ec7bda51babc967961ba
MARINE GUEGUEN <gueguenmarine.p7@gmail.com>
2022-03-16 14:15:07 +0100

    ras

--- pages/index.php
+++ pages/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
     <link href="/assets/main.css" rel="stylesheet">
-    
+
 </head>
 <body>
     <?php
