bccfed975b642fdbdc0f0aa45f504ccb24ddea4b
MARINE GUEGUEN <gueguenmarine.p7@gmail.com>
2022-03-16 11:45:14 +0100

    include navbar with php + debug navbar

--- layouts/_header.php
+++ layouts/_header.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="/assets/nav.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
     </style> 
@@ -23,12 +23,12 @@
             <input type="checkbox">
             <span class="menu"> <span class="hamburger"></span> </span>
             <ul>
-                <li> <a href="#">Accueil</a> </li>
-                <li> <a href="#">Apéritifs</a> </li>
-                <li> <a href="#">Entrées</a> </li>
-                <li> <a href="#">Plats</a> </li>
-                <li> <a href="#">Desserts</a> </li>
-                <li> <a href="#">Ta recette ?</a> </li>
+                <li> <a href="/pages/index.php">Accueil</a> </li>
+                <li> <a href="/pages/rubrics.php">Apéritifs</a> </li>
+                <li> <a href="/pages/rubrics.php">Entrées</a> </li>
+                <li> <a href="/pages/rubrics.php">Plats</a> </li>
+                <li> <a href="/pages/rubrics.php">Desserts</a> </li>
+                <li> <a href="/pages/form.php">Ta recette ?</a> </li>
             </ul>
 
         </label>
--- pages/recipes.php
+++ pages/recipes.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Recipes</title>
 </head>
 <body>
+<?php
+        include '/home/marine/Documents/wild-food/layouts/_header.php';
+    ?>
+
+    <h1>Recipes</h1>
+
+
+
     
 </body>
 </html>
