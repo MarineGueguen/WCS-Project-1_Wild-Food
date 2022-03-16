9f28ef32023e882831f6b155ac03140349998276
alainaissi <89066710+alainaissi@users.noreply.github.com>
2022-03-16 11:52:17 +0100

    1st shot rubrics page
    Link to rubrics.css
    Link to _header.php
    Link to _footer.php

--- rubrics.php
+++ rubrics.php
@@ -4,9 +4,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wild Food</title>
+    <link rel="stylesheet" href="rubrics.css">
 </head>
 <body>
     
+    <?php include ‘_header.php’; ?>
+
+<div class="site-container">
+    <div class="article-container">
+        <article class="article-card">
+            <figure class="article-image">
+                <img src="https://i.ibb.co/tsXMbBL/sebastian-coman-photography-ZGg-IYa-L9l-Yk-unsplash.jpg" alt="ting-tian-E6-Dsqn-Zb-Z4o-unsplash" alt="Appetizer Section"
+                />
+            </figure>
+            <div class="article-content">
+                <a href="#" class="card-category">Food</a>
+                <h3 class="card-title">Apéritifs</h3>
+                <p class="card-excerpt">Le meilleur des apéritifs à partager entre amis. 
+                    <br>Verrines, toasts, amuse-gueules ou petits feuilletées, surprenez vos convives !</p>
+            </div>    
+        </article>    
+        <article class="article-card">
+            <figure class="article-image">
+                <img src="https://i.ibb.co/QnJRGZN/joseph-gonzalez-fdl-ZBWIP0a-M-unsplash.jpg" alt="Starter Section"
+                />
+            </figure>
+            <div class="article-content">
+                <a href="#" class="card-category">Food</a>
+                <h3 class="card-title">Entrées</h3>
+                <p class="card-excerpt">Découvrez nos recettes faciles d'entrées maison pour commencer le repas en beauté.</p>
+            </div>    
+        </article>   
+        <article class="article-card">
+            <figure class="article-image">
+                <img src="https://i.ibb.co/YXqK1kx/caroline-attwood-bp-PTl-XWTOvg-unsplash.jpg" alt="Main Course Section"
+                />
+            </figure>
+            <div class="article-content">
+                <a href="#" class="card-category">Food</a>
+                <h3 class="card-title">Plats</h3>
+                <p class="card-excerpt">Du repas léger au plus décadent, nos recettes de plats principaux sont variées et savoureuses !</p>
+            </div>    
+        </article>   
+        <article class="article-card">
+            <figure class="article-image">
+                <img src="https://i.ibb.co/S7B5TcY/anna-tukhfatullina-food-photographer-stylist-Mzy-Ojt-CI70-unsplash.jpg" alt="Dessert Section"
+                />
+            </figure>
+            <div class="article-content">
+                <a href="#" class="card-category">Food</a>
+                <h3 class="card-title">Desserts</h3>
+                <p class="card-excerpt">Impressionez vos invités avec l'une de nos recettes sucrées.
+                    <br>Des plus gourmandes aux plus fines, ces pâtisseries ne feront qu'un avec votre diabète.</p>
+            </div>    
+        </article>   
+    </div>    
+</div>
+
+    <?php include ‘_footer.php’; ?>
+
 </body>
 </html>
