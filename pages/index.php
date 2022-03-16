df94f08b5dc6845b148c6f495303b3f15dadcd8f
Nolan Prévost <nolan.prevost@gmail.com>
2022-03-16 15:13:37 +0100

    creation of index and footer + css

--- pages/index.php
+++ pages/index.php
@@ -1,22 +1,28 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-    <link href="../assets/main.css" rel="stylesheet">
-    
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="Styles/welcome.css" rel="stylesheet" />
 </head>
 <body>
-    <?php
-        include '../layouts/_header.php';
-    ?>
+  <section>
+    <article class="borderRadius aperitifs"><a href="#"><h2>Apéritifs</h2></a></article>
 
-    <h1>Accueil</h1>
+    <article class="borderRadius entrees"><a href="#"><h2>Entrées</h2></a></article>
+
+    <article class="borderRadius plats"><a href="#"><h2>Plats</h2></a></article>
+
+    <article class="borderRadius desserts"><a href="#"><h2>Desserts</h2></a></article>
+  </section>
 
 
 
-
+  <?php
+  include "Layouts/footer.php";
+  ?>
 </body>
 </html>
+
