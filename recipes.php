b99640bcadb85cf21734203450b3fc88784415d1
Florian COURTAIGNE <f.courtaigne@gmail.com>
2022-03-16 14:00:55 +0100

    Updating recipes files

--- recipes.php
+++ recipes.php
@@ -3,11 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="recipes.css">
     <title>Recettes</title>
 </head>
-<header>
-    <h1>Header</h1>
-</header>
+<header></header>
 <body>
+    <main>
+
+        <section class="recipe_image_container">
+            <div class="recipe_image_box">
+                <img src="https://picsum.photos/200" alt="gateau_au_chocolat"/>
+            </div>   
+        </section>
+        <section class="recipe_title">
+            <h1>Gateau au chocolat</h1>
+        </section>
+        <section class="recipe_ingredients">
+            <p>Ingrédients : </p>
+            <ul>
+                <li>100g de beurre</li>
+                <li>200g d sucre</li>
+                <li>100g de chocolat</li>
+                <li>4 oeufs</li>
+                <li>50g de poudre d'amande</li>
+                <li>20cl de crème fraîche</li>
+            </ul>
+        </section>
+        <section class="recipe_steps">
+            <div class="step1">
+                <p>Etape 1</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae sint placeat aliquam quaerat dolorem beatae eum, ratione reprehenderit architecto voluptate eius temporibus, dolore nostrum unde. Inventore iure optio laborum.</p>
+            </div>
+            <div class="step2">
+                <p>Etape 2</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae sint placeat aliquam quaerat dolorem beatae eum, ratione reprehenderit architecto voluptate eius temporibus, dolore nostrum unde. Inventore iure optio laborum.</p>
+            </div>
+            <div class="step3">
+                <p>Etape 3</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae sint placeat aliquam quaerat dolorem beatae eum, ratione reprehenderit architecto voluptate eius temporibus, dolore nostrum unde. Inventore iure optio laborum.</p>
+            </div>
+            <div class="step4">
+                <p>Etape 4</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae sint placeat aliquam quaerat dolorem beatae eum, ratione reprehenderit architecto voluptate eius temporibus, dolore nostrum unde. Inventore iure optio laborum.</p>
+            </div>
+            <div class="step5">
+                <p>Etape 5</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae sint placeat aliquam quaerat dolorem beatae eum, ratione reprehenderit architecto voluptate eius temporibus, dolore nostrum unde. Inventore iure optio laborum.</p>
+            </div>
+            <div class="step6">
+                <p>Etape 6</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae sint placeat aliquam quaerat dolorem beatae eum, ratione reprehenderit architecto voluptate eius temporibus, dolore nostrum unde. Inventore iure optio laborum.</p>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
