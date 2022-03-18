8e5d136a3a24e26645d3509594d854af780956aa
Nolan Prévost <nolan.prevost@gmail.com>
2022-03-18 09:16:05 +0100

    ain.css updated

--- pages/index.php
+++ pages/index.php
@@ -6,6 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link href="../assets/welcome.css" rel="stylesheet" />
+  <link href="../assets/main.css" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">  
 </head>
 <body>
 <?php
@@ -16,6 +20,7 @@
       <a href="#" >
         <img class="homePageImage" src="https://placeimg.com/640/480/nature"/>
         <h2 class="homePageTitle">Apéritifs</h2>
+        <p class="aperitifSubtitle">Le meilleur des apéritifs à partager entre amis. Verrines, toasts, amuse-gueules ou petits feuilletées, surprenez vos convives !</p>
       </a>
     </article>
     <article>
