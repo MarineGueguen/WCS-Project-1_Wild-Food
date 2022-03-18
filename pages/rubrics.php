505fa0d59c43c24cce2c71fb101d5a96c20780b6
alainaissi <alain-aissi_student2022@wilder.school>
2022-03-18 09:20:28 +0100

    added img + updated rubrics php & css

--- pages/rubrics.php
+++ pages/rubrics.php
@@ -4,67 +4,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="rubrics.css">
+    <link rel="stylesheet" href="../assets/rubrics.css">
     <title>Rubrics</title>
 </head>
+
 <body>
     <?php
         include '../layouts/_header.php';
     ?>
 
-    <h1>Rubrics</h1>
 <div class="site-container">
     <div class="article-container">
         <article class="article-card">
             <figure class="article-image">
-                <img src="https://i.ibb.co/tsXMbBL/sebastian-coman-photography-ZGg-IYa-L9l-Yk-unsplash.jpg" alt="ting-tian-E6-Dsqn-Zb-Z4o-unsplash" alt="Appetizer Section"
+                <img src="../assets/images/parisBrest.jpeg" alt="Dessert Paris Brest"
                 />
             </figure>
             <div class="article-content">
-                <a href="#" class="card-category">Food</a>
-                <h3 class="card-title">Apéritifs</h3>
-                <p class="card-excerpt">Le meilleur des apéritifs à partager entre amis. 
-                    <br>Verrines, toasts, amuse-gueules ou petits feuilletées, surprenez vos convives !</p>
+                <a href="recipes.php" class="card-category">Desserts</a>
+                <h3 class="card-title">Paris Brest</h3>
+                <p class="card-excerpt"></p>
             </div>    
         </article>    
         <article class="article-card">
             <figure class="article-image">
-                <img src="https://i.ibb.co/QnJRGZN/joseph-gonzalez-fdl-ZBWIP0a-M-unsplash.jpg" alt="Starter Section"
+                <img src="../assets/images/tarteCitron.jpeg" alt="Dessert Tarte Citron Meringuée"
                 />
             </figure>
             <div class="article-content">
-                <a href="#" class="card-category">Food</a>
-                <h3 class="card-title">Entrées</h3>
-                <p class="card-excerpt">Découvrez nos recettes faciles d'entrées maison pour commencer le repas en beauté.</p>
+                <a href="recipes.php" class="card-category">Desserts</a>
+                <h3 class="card-title">Tarte au citron meringuée</h3>
+                <p class="card-excerpt"></p>
             </div>    
         </article>   
         <article class="article-card">
             <figure class="article-image">
-                <img src="https://i.ibb.co/YXqK1kx/caroline-attwood-bp-PTl-XWTOvg-unsplash.jpg" alt="Main Course Section"
+                <img src="../assets/images/bucheNoel.jpeg" alt="Dessert Buche Noël à l'ancienne"
                 />
             </figure>
             <div class="article-content">
-                <a href="#" class="card-category">Food</a>
-                <h3 class="card-title">Plats</h3>
-                <p class="card-excerpt">Du repas léger au plus décadent, nos recettes de plats principaux sont variées et savoureuses !</p>
+                <a href="recipes.php" class="card-category">Desserts</a>
+                <h3 class="card-title">Bûche de Noël à l'ancienne</h3>
+                <p class="card-excerpt"></p>
             </div>    
         </article>   
         <article class="article-card">
             <figure class="article-image">
-                <img src="https://i.ibb.co/S7B5TcY/anna-tukhfatullina-food-photographer-stylist-Mzy-Ojt-CI70-unsplash.jpg" alt="Dessert Section"
+                <img src="../assets/images/royalChocolat.jpeg" alt="Dessert Royal Chocolat"
                 />
             </figure>
             <div class="article-content">
-                <a href="#" class="card-category">Food</a>
-                <h3 class="card-title">Desserts</h3>
-                <p class="card-excerpt">Impressionez vos invités avec l'une de nos recettes sucrées.
-                    <br>Des plus gourmandes aux plus fines, ces pâtisseries ne feront qu'un avec votre diabète.</p>
+                <a href="recipes.php" class="card-category">Desserts</a>
+                <h3 class="card-title">Royal Chocolat</h3>
+                <p class="card-excerpt"></p>
             </div>    
         </article>   
     </div>    
 </div>
+
     <?php
-        include '../layouts/_header.php';
+        include '../layouts/_footer.php';
     ?>
+
 </body>
 </html>
